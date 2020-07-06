25a586a078a9322755e3d4065e6d050628983544
mcdonnello <67693146+mcdonnello@users.noreply.github.com>
2020-07-06 12:37:33 +0100

    Have gathered clickhouse return data in string

--- select_query.py
+++ select_query.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from aiohttp import ClientSession
 import json
 from query_builder import QueryBuilder
 async def select_query(request):
@@ -8,9 +9,15 @@ async def select_query(request):
         limit = request.query['limit']
         query_builder = QueryBuilder()
         query_string = query_builder.build_select(columns, table_name, limit)
-        response_obj = { 'status' : 'query successful', 'query' : query_string }
+
+        clickhouse_http_query = "http://mcdonnelloprojects.xyz:8123//?query={0}".format(query_string)
+        async with ClientSession() as session:
+            async with session.get(clickhouse_http_query) as resp:
+                response_data = await resp.text()
+        await session.close()
+
         # return a success json response with status code 200 i.e. 'OK'
-        return web.Response(text=json.dumps(response_obj), status=200)
+        return web.Response(text=json.dumps(response_data), status=200)
     except Exception as e:
         response_obj = { 'status' : 'failed', 'reason': str(e) }
         # return failed with a status code of 500 i.e. 'Server Error'
